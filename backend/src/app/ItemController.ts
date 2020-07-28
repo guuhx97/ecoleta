1f2fb311b8c7436aee6f727ba27ebb5562cc71bd
guuhx97 <gustavorobertotb@outlook.com>
2020-07-27 22:19:04 -0300

    :iphone: Feat: Iniciado desenvolvimento do app mobile

--- backend/src/app/ItemController.ts
+++ backend/src/app/ItemController.ts
@@ -7,7 +7,7 @@ class ItemController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.0.120:3333/uploads/${item.image}`,
       };
     });
     return response.json(serializedItems);
